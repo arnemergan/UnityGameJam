4c5fdb983179fea665b62be45161c9779e47a870
arnemergan <mergan.arne@hotmail.com>
2020-01-29 09:51:44 +0100

    initial commit

---
+++ GameJam/Assets/scripts/enemyMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyMovement : MonoBehaviour
+{
+    public float lookRadius = 10;
+
+    public Transform target;
+    NavMeshAgent navMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position,transform.position);
+        if(distance <= lookRadius){
+            navMesh.SetDestination(target.position);
+            if(distance <= navMesh.stoppingDistance){
+                //do damage to player
+            }
+        }
+    }
+}
---
+++ GameJam/Assets/scripts/playerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float moveSpeed;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        moveInput = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+        if(Input.GetAxis("Mouse X") != 0 && Input.GetAxis("Mouse Y") != 0){
+            Vector3 lookDirection = new Vector3(Input.GetAxis("Mouse X") / 25, 0, Input.GetAxis("Mouse Y") / 25);
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+        }
+        rb.velocity = moveVelocity;
+    }
+}
