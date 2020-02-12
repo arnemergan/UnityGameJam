a045b5e57efd3ce4a092014fa2ba2e8fb3c9b0a8
IljaDR <38908439+IljaDR@users.noreply.github.com>
2020-02-12 01:10:38 +0100

    Started rewriting zombie logic

--- GameJam/Assets/scripts/enemyMovement.cs
+++ GameJam/Assets/scripts/enemyMovement.cs
@@ -1,64 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+﻿using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     public float lookRadius = 10;
-    private Transform target1;
-    private Transform target2;
-    private GameObject playertarget1;
-    private GameObject playertarget2;
-    public Enemy enemy;
-    private Transform[] transforms = new Transform[2];
+    public int soldierNumber;
+    private GameObject target;
+    private Enemy enemy;
+    private Transform targetLocation;
+    private Rigidbody rb;
 
-    //NavMeshAgent navMesh;
-    // Start is called before the first frame update
     void Start()
     {
-        playertarget1 = GameObject.Find("soldier1");
-        playertarget2 = GameObject.Find("soldier2");
-        target1 = playertarget1.transform;
-        target2 = playertarget2.transform;
-        transforms[0] = target1;
-        transforms[1] = target2;
         enemy = GetComponent<Enemy>();
-        //navMesh = GetComponent<NavMeshAgent>();
+
+        rb = enemy.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        target = GameObject.Find("soldier" + soldierNumber);
+        targetLocation = target.transform;
+        transform.LookAt(targetLocation);
+        transform.rotation *= Quaternion.Euler(0, -90, 0);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-
-        /*Transform target = GetClosestTarget(transforms);
-        if(enemy.health > 0){
-            float distance = Vector3.Distance(target.position,transform.position);
-            if(distance <= lookRadius){
-                //navMesh.SetDestination(target.position);
-                if(distance <= navMesh.stoppingDistance){
-                    //do damage to player
-                    Player player = target.GetComponent<Player>();
-                    player.TakeDamage(enemy.damage,target); 
-                }
-            }
-        }*/
+        rb.velocity = transform.forward * 1000;
     }
-
-Transform GetClosestTarget(Transform[] targets)
-{
-    Transform tMin = null;
-    float minDist = Mathf.Infinity;
-    Vector3 currentPos = transform.position;
-    foreach (Transform t in targets)
-    {
-        float dist = Vector3.Distance(t.position, currentPos);
-        if (dist < minDist)
-        {
-            tMin = t;
-            minDist = dist;
-        }
-    }
-    return tMin;
-}
 }
